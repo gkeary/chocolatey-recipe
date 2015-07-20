bcbfa1416186859b8d9338539d890349430fc221
Gregg Keary <gregg@gkeary.com>
2015-07-20 13:00:37 -0400

    Added undo .txt and RunBoxstarterRevers.bat
    see how it goes

--- RunBoxstarterInstall.bat
+++
@@ -1,1 +0,0 @@
-START http://boxstarter.org/package/url?https://raw.githubusercontent.com/gkeary/chocolatey-recipe/master/chocolatey-recipe.txt 
---
+++ RunBoxstarterREVERSE.bat
@@ -0,0 +1,1 @@
+START http://boxstarter.org/package/url?https://raw.githubusercontent.com/gkeary/chocolatey-recipe/master/undo.txt
